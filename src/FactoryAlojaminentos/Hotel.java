1229e4c03a06fcda39ca1f9e7cd6a723da550664
RobertoAlc <33253474+RobertoAlc@users.noreply.github.com>
2020-01-16 17:44:59 +0100

    crear paquete factory

---
+++ src/FactoryAlojaminentos/Hotel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryAlojaminentos;
+
+/**
+ *
+ * @author Roberto
+ */
+public class Hotel extends Alojamiento {
+     int estrellas;
+     boolean pensionCompleta;
+
+    public Hotel(String id, String direccion, int plazas, boolean mascotas, boolean discapacitados, float precio_noche, int estrellas, boolean pensionCompleta) {
+        super(id, direccion, plazas, mascotas, discapacitados, precio_noche);
+        this.estrellas = estrellas;
+        this.pensionCompleta = pensionCompleta;
+    }
+
+    public int getEstrellas() {
+        return estrellas;
+    }
+
+    public void setEstrellas(int estrellas) {
+        this.estrellas = estrellas;
+    }
+
+    public boolean tienePensionCompleta() {
+        return pensionCompleta;
+    }
+
+    public void setPensionCompleta(boolean pensionCompleta) {
+        this.pensionCompleta = pensionCompleta;
+    }
+     
+     
+    
+}
