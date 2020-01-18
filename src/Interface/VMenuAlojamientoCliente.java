16a41fb7ab9966c6ba4ec4eb5b62c2fd76636c3e
Carlos <karlosssanta@gmail.com>
2020-01-19 00:11:16 +0100

    Añadir consultar Reservas

--- src/Interface/VMenuAlojamientoCliente.java
+++ src/Interface/VMenuAlojamientoCliente.java
@@ -110,6 +110,9 @@ public class VMenuAlojamientoCliente extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        ConsultarReservas cr = new ConsultarReservas(this, cliente);
+        cr.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
